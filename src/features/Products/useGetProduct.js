57da21c10224445583c87bf52d7495f7a1c6fb12
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-08-01 13:21:33 -0300

    Product detail hook

---
+++ src/features/Products/useGetProduct.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@tanstack/react-query';
+import { getProduct } from '../../apiServices/productsService';
+import { useParams } from 'react-router-dom';
+
+export const useGetProduct = () => {
+  const { productId } = useParams();
+
+  const {
+    data: product,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ['product'],
+    queryFn: () => getProduct(productId),
+  });
+
+  return { product, isLoading, error };
+};
