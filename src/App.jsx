2db092c08b5fc9145f2a393e3c8d5192ce62fe94
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-08-06 17:33:01 -0300

    Code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,26 @@ import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from 'react-hot-toast';
-import AppLayout from './pages/dashboard/AppLayout';
-import Dashboard from './pages/dashboard/Dashboard';
-import Categories from './pages/dashboard/Categories';
-import Discounts from './pages/dashboard/Discounts';
-import Orders from './pages/dashboard/Orders';
-import Products from './pages/dashboard/Products';
-import Login from './pages/Login';
+
 import NotFound from './pages/NotFound';
 import Styles from './styles/Styles';
-import ProductDetails from './features/Products/ProductDetails';
-import OrderDetails from './features/Orders/OrderDetails';
-import Tickets from './pages/dashboard/Tickets';
-import Settings from './pages/dashboard/Settings';
-import Home from './pages/store/Home';
 import ProtectedRoute from './features/Authentication/ProtectedRoute';
-import NewUser from './pages/dashboard/NewUser';
+import { lazy, Suspense } from 'react';
+import FullScreenSpinner from './components/FullScreenSpinner';
+
+const Home = lazy(() => import('./pages/store/Home'));
+const Login = lazy(() => import('./pages/Login'));
+const AppLayout = lazy(() => import('./pages/dashboard/AppLayout'));
+const Dashboard = lazy(() => import('./pages/dashboard/Dashboard'));
+const Products = lazy(() => import('./pages/dashboard/Products'));
+const ProductDetails = lazy(() => import('./features/Products/ProductDetails'));
+const Orders = lazy(() => import('./pages/dashboard/Orders'));
+const OrderDetails = lazy(() => import('./features/Orders/OrderDetails'));
+const Settings = lazy(() => import('./pages/dashboard/Settings'));
+const NewUser = lazy(() => import('./pages/dashboard/NewUser'));
+const Discounts = lazy(() => import('./pages/dashboard/Discounts'));
+const Tickets = lazy(() => import('./pages/dashboard/Tickets'));
+const Categories = lazy(() => import('./pages/dashboard/Categories'));
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -33,39 +37,41 @@ function App() {
       <ReactQueryDevtools />
       <Styles />
       <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='login' element={<Login />} />
-          <Route path='*' element={<NotFound />} />
-          <Route
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          >
+        <Suspense fallback={<FullScreenSpinner />}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='login' element={<Login />} />
+            <Route path='*' element={<NotFound />} />
             <Route
-              path='/admin'
-              element={<Navigate replace to='/admin/dashboard' />}
-            />
-            <Route path='/admin/dashboard' element={<Dashboard />} />
-            <Route path='/admin/categories' element={<Categories />} />
-            <Route path='/admin/discounts' element={<Discounts />} />
-            <Route path='/admin/orders' element={<Orders />} />
-            <Route
-              path='admin/order/details/:orderId'
-              element={<OrderDetails />}
-            />
-            <Route path='/admin/new-user' element={<NewUser />} />
-            <Route path='/admin/tickets' element={<Tickets />} />
-            <Route path='/admin/my-store' element={<Settings />} />
-            <Route path='admin/products' element={<Products />} />
-            <Route
-              path='admin/products/variants/:productName'
-              element={<ProductDetails />}
-            />
-          </Route>
-        </Routes>
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route
+                path='/admin'
+                element={<Navigate replace to='/admin/dashboard' />}
+              />
+              <Route path='/admin/dashboard' element={<Dashboard />} />
+              <Route path='/admin/categories' element={<Categories />} />
+              <Route path='/admin/discounts' element={<Discounts />} />
+              <Route path='/admin/orders' element={<Orders />} />
+              <Route
+                path='admin/order/details/:orderId'
+                element={<OrderDetails />}
+              />
+              <Route path='/admin/new-user' element={<NewUser />} />
+              <Route path='/admin/tickets' element={<Tickets />} />
+              <Route path='/admin/my-store' element={<Settings />} />
+              <Route path='admin/products' element={<Products />} />
+              <Route
+                path='admin/products/variants/:productName'
+                element={<ProductDetails />}
+              />
+            </Route>
+          </Routes>
+        </Suspense>
       </BrowserRouter>
       <Toaster
         position='bottom-right'
