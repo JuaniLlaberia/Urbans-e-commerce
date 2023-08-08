a35f80653963be20b38c177381f665b94ecd5a77
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-08-08 10:45:12 -0300

    Reusable product list

--- src/features/StoreProducts/Products.jsx
+++ src/features/StoreProducts/Products.jsx
@@ -2,33 +2,8 @@ import Title from '../../components/Title';
 import Spinner from '../../components/Spinner';
 import { useParams, useSearchParams } from 'react-router-dom';
 import { useGetProductsByCategory } from '../Products/useGetProductsByCategory';
-import { styled } from 'styled-components';
 import ProductItem from './ProductItem';
-
-//Try to make it cleanner
-const StyledProductsList = styled.ul`
-  margin-top: 25px;
-  margin-left: 12.5px;
-  display: grid;
-  grid-template-columns: repeat(6, 1fr);
-  list-style: none;
-  row-gap: 1rem;
-  @media (max-width: 1350px) {
-    grid-template-columns: repeat(5, 1fr);
-  }
-  @media (max-width: 1170px) {
-    grid-template-columns: repeat(4, 1fr);
-  }
-  @media (max-width: 970px) {
-    grid-template-columns: repeat(3, 1fr);
-  }
-  @media (max-width: 670px) {
-    grid-template-columns: repeat(2, 1fr);
-  }
-  @media (max-width: 490px) {
-    grid-template-columns: repeat(1, 1fr);
-  }
-`;
+import StyledProductsList from './ProductList';
 
 const Products = () => {
   const { mainCategory } = useParams();
