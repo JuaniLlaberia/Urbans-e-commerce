55c872b4d16834203a4adb352602d02e77cb8d1d
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-08-09 17:00:49 -0300

    Created logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,5 +1,15 @@
+import { useThemeContext } from '../context/ThemeContext';
+import logoLight from '../../public/logo-light.png';
+import logoDark from '../../public/logo-dark.png';
+import { styled } from 'styled-components';
+
+const Img = styled.img`
+  width: 110px;
+`;
+
 const Logo = () => {
-  return <h1>Logo</h1>;
+  const { theme } = useThemeContext();
+  return <Img src={theme === 'light' ? logoLight : logoDark} />;
 };
 
 export default Logo;
