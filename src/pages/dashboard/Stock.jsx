59cd2fbdaa78a15d2f56dd3081f381b1b5e813ee
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-08-09 17:05:11 -0300

    Added stock page

---
+++ src/pages/dashboard/Stock.jsx
@@ -0,0 +1,24 @@
+import OrderBy from '../../components/OrderBy';
+import Row from '../../components/Row';
+import Title from '../../components/Title';
+import AddStock from '../../features/Products/AddStock';
+import StockTable from '../../features/Products/StockTable';
+import Top from '../../components/Top';
+import { orderStock } from '../../utils/orderConsts';
+
+const Stock = () => {
+  return (
+    <>
+      <Top>
+        <Title as='h2'>Stock</Title>
+      </Top>
+      <Row>
+        <OrderBy options={orderStock} />
+        <AddStock />
+      </Row>
+      <StockTable />
+    </>
+  );
+};
+
+export default Stock;
