6771e421c5d2ec1afa83f772d8fd5705823e160e
JuaniLlaberia <juanillaberia2002@gmail.com>
2023-08-13 18:25:34 -0300

    Cart items number in navbar

--- src/components/NavbarButtons.jsx
+++ src/components/NavbarButtons.jsx
@@ -5,6 +5,8 @@ import {
   HiOutlineMagnifyingGlass,
 } from 'react-icons/hi2';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getCartLength } from '../features/Cart/cartSlice';
 
 const ButtonsContainer = styled.div`
   display: flex;
@@ -18,9 +20,26 @@ const ButtonsContainer = styled.div`
 
 const StyledLink = styled(Link)`
   color: var(--color-white-5);
+  position: relative;
+`;
+
+const StyledNumber = styled.p`
+  position: absolute;
+  top: -7.5px;
+  right: -5px;
+  background-color: var(--icons-color);
+  color: #f8f4f4;
+  font-weight: 600;
+  font-size: 0.8rem;
+  width: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 25px;
 `;
 
 const NavbarButtons = () => {
+  const cartLength = useSelector(getCartLength);
   return (
     <ButtonsContainer>
       <StyledLink to='/products/saved'>
@@ -28,6 +47,7 @@ const NavbarButtons = () => {
       </StyledLink>
       <StyledLink to='/cart'>
         <HiOutlineShoppingCart />
+        {cartLength > 0 && <StyledNumber>{cartLength}</StyledNumber>}
       </StyledLink>
       <HiOutlineMagnifyingGlass />
     </ButtonsContainer>
